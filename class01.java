d57b4fa7109ace17d30528faf065bf8fe066f5e6
Xian0328 <91442683+Xian0328@users.noreply.github.com>
2021-11-01 12:07:04 +0800

    Add files via upload

---
+++ class01.java
@@ -0,0 +1,40 @@
+class Cal_area
+{
+	public double pi=3.14159;
+	public int radius;
+	public int width;
+	public int height;
+	public int length;
+	public Cal_area(int r,int w,int h,int l)
+	{
+		radius=r;
+		width=w;
+		height=h;
+		length=l;
+	}
+	double Area1()
+	{
+		return 4.0*pi*radius*radius*radius/3.0;
+	}
+	int Area2() 
+	{
+		return (2*width*length)+(2*length*height)+(2*width*height);
+	}
+	public void show()
+	{
+		System.out.println("半徑="+radius);
+		System.out.println("球型體積="+Math.round(Area1()*10.0)/10.0);
+		System.out.println(" ");
+		System.out.println("長="+length+",寬="+width+",高="+height);
+		System.out.println("立方體表面積="+Area2());
+	}
+}
+public class class01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cal_area obj=new Cal_area(10,5,6,4);
+		obj.show();
+	}
+
+}
---
+++ class02.java
@@ -0,0 +1,31 @@
+class is_prime
+{
+	public int num;
+	public is_prime(int n)
+	{
+		num=n;
+	}
+	public void show()
+	{
+		for(int i=2;i<num;i++)
+		{
+			if (num%i==0) {
+				System.out.println("NO");
+				break;
+			}
+			else {
+				System.out.println("YES");
+				break;
+			}
+		}
+	}
+}
+public class class02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		is_prime obj=new is_prime(20);
+		obj.show();
+	}
+
+}
